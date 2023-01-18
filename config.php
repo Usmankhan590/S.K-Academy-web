fc971fb564cb25d47eff0e46f07b1efe64599b23
Usmankhan590 <usmanazizkhan5@gmail.com>
2023-01-18 05:14:54 +0500

    S.k-Academy-web

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("ServerName","localhost");
+define("ServerUser","root");
+define("ServerPass","");
+define("ServerDB","data_entry");
+
+$conn = mysqli_connect(ServerName,ServerUser,ServerPass,ServerDB);
+?>
